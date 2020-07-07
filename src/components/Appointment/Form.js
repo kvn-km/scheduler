efe403336ee172ef6914b066d1f3f84156984ffe
Kevin Kim <kevinfkim@gmail.com>
2020-07-07 16:52:54 -0400

    form buttons for cancel and save execute functions for reset the form or display the information of the interviewer id and the name of the student from the input.

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -8,10 +8,24 @@ export default function Form(props) {
   let [name, setName] = useState(props.name || "");
   let [interviewer, setInterviewer] = useState(props.interviewer || null);
 
+  const resetForm = () => {
+    setName('');
+    setInterviewer(null);
+  };
+
+  const cancel = () => {
+    resetForm();
+    props.onCancel();
+  };
+
+  const save = () => {
+    props.onSave(name, interviewer);
+  };
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
@@ -32,8 +46,8 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>Cancel</Button>
-          <Button confirm onClick={props.onSave}>Save</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={save}>Save</Button>
         </section>
       </section>
     </main>
