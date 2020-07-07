d59d772ec6bbe9387f1cf3b46a09a710f4e0adbc
Kevin Kim <kevinfkim@gmail.com>
2020-07-07 17:16:51 -0400

    Application components complete.

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { Fragment } from "react";
 // import classNames from "classnames";
 
 import "./styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+
 
 export default function Appointment(props) {
   return (
     <article className="appointment">
-      Hello{props.time && `. The time is ${props.time}`}.
+      <Header time={props.time} />
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
     </article>
   );
 }
