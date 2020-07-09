791cbf1bbf17a3e00e94331ae9c6f37084008a75
Kevin Kim <kevinfkim@gmail.com>
2020-07-08 20:42:51 -0400

    create custom hook to use in the appointment component. dealing with modes and going back and forth between modes, as well as mode history.

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,13 +1,11 @@
 
 export function getAppointmentsForDay(state, day) {
-
   let theState = {};
   for (let days of state.days) {
     if (days.name === day) {
       theState = days.appointments;
     }
   }
-
   let daysAppointments = [];
   if (theState.length > 0) {
     for (let appointment of theState) {
@@ -18,19 +16,13 @@ export function getAppointmentsForDay(state, day) {
 }
 
 export function getInterview(state, interview) {
-
   let theInterview = {};
-
   if (interview === null) {
     return null;
   }
-  const interviewerId = interview.interviewer;
-
   theInterview = {
     ...interview,
-    interviewer: state.interviewers[interviewerId]
+    interviewer: state.interviewers[interview.interviewer]
   };
-
   return theInterview;
-
 }
---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+export default function useVisualMode(init) {
+
+  const [mode, setMode] = useState(init);
+  const [history, setHistory] = useState([init]);
+
+  function transition(init, replace = false) {
+    if (replace) {
+      history.pop();
+      setMode(init);
+      history.push(init);
+    } else {
+      setMode(init);
+      history.push(init);
+    }
+  }
+
+  function back() {
+    if (history.length > 1) {
+      history.pop();
+      setMode(history[history.length - 1]);
+    }
+  }
+
+  return { mode, transition, back };
+}
