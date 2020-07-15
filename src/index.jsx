2a2071c8f3a81696fa113e754527a9642c5892b2
Kevin Kim <kevinfkim@gmail.com>
2020-07-15 18:10:38 -0400

    add missing axios import to index.jsx.

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import axios from "axios";
+
 if (process.env.REACT_APP_API_BASE_URL) {
   axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
 }
